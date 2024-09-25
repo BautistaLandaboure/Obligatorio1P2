68be4d2f64a12bddd4391bb37635fc045160e1ed
macbookpro <macbookpro@192.168.1.8>
2024-09-25 15:25:41 -0300

    agregue Clase RegistrosUsuarios

---
+++ src/obligatorio1/RegistroUsuarios.java
@@ -0,0 +1,58 @@
+package obligatorio1;
+
+public class RegistroUsuarios {
+    
+    private String nombre;
+    
+    private int edad;
+    
+    private String alias;
+
+    // Constructor para crear una instancia de usuario
+    public RegistroUsuarios(String nombre, int edad, String alias) {
+        
+        this.nombre = nombre;
+        
+        this.edad = edad;
+        
+        this.alias = alias;
+    }
+
+    // Obtener el alias del usuario
+   
+
+    // Método para mostrar la información del usuario
+    @Override
+    public String toString() {
+        
+        return "Nombre: " + nombre + ", Edad: " + edad + ", Alias: " + alias;
+        
+    }
+    
+     public String getAlias() {
+        return alias; 
+    }
+     
+     public void setAlias() {
+         this.alias = alias;
+     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+}
