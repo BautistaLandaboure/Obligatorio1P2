cd13a944e8803a5e1e0643e055469535acbd5c5d
macbookpro <macbookpro@192.168.1.8>
2024-09-25 15:27:52 -0300

    agregue Clase RegistrosUsuarios

--- src/obligatorio1/Obligatorio1.java
+++ src/obligatorio1/Obligatorio1.java
@@ -1,11 +1,12 @@
 package obligatorio1;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
 public class Obligatorio1 {
 
     public static void main(String[] args) {
         
-        System.out.println("HOLA");
-      
     }
-    
 }
